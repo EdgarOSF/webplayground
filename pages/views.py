cb6011540e22af6e77ecac863743c9f13665f57e
Omar <edgaromarsf@gmail.com>
2020-05-08 14:31:54 -0500

    segundo commit

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Page
+from django.views.generic.list import ListView
 
 # Create your views here.
 def pages(request):
