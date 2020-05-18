49b53cfc9c26088888c2f486bff3dfad0a6f7d70
Edgar Omar <edgaromarsf@gmail.com>
2020-05-17 23:19:27 -0500

    decoradores

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.admin.views.decorators import staff_member_required
 from .views import PageListView, PageDetailView, PageCreate, PageUpdate, PageDelete
 
 pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:page_slug>/', PageDetailView.as_view(), name='page'),
-    path('create/', PageCreate.as_view(), name = 'create'),
-    path('update/<int:pk>', PageUpdate.as_view(), name = 'update'),
-    path('delete/<int:pk>', PageDelete.as_view(), name = 'delete')
+    path('create/', staff_member_required(PageCreate.as_view()), name = 'create'),
+    path('update/<int:pk>', staff_member_required(PageUpdate.as_view()), name = 'update'),
+    path('delete/<int:pk>', staff_member_required(PageDelete.as_view()), name = 'delete')
 ], 'pages') # para acceder a las vistas: pages:pages, pages:page, pages:create
